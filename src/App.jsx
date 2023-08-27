2eceb9fa988ac66f1719f1b05998010c816158d5
Azikenp <azikenp26@gmail.com>
2023-08-27 17:08:18 +0100

    additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,63 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Markup from './components/Markup'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Markup from "./components/Markup";
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [word, setWord] = useState('');
 
-  const [inputs, setInputs] = useState('rat');
-  const [word, setWord] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputs}`)
-    .then(res => res.json())
-    .then(data => setWord(data[0]))
-    .catch(err => console.error(`haaaa ${err}`))
-  }, []);
-
-  console.log(word);
-
-  function getMeaning(){
-    const meaning = word;
-    setWord(meaning)
+  const searchResults = (inputValue) => {
+    return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
+      .then((response) => response.json())
+      .then((data) => {
+        return setWord(data[0])
+      })
+      .catch((err) => console.error(`haaaa ${err}`));
   };
 
-  const handleChange = (event) => {
-    event.preventDefault()
-    setInputs(event.target.value);
-  };
-  const playSound = () => {
-    sound.play();
-  };
 
   const searchFunction = (e) => {
     e.preventDefault();
-    // clearView();
-    // renderSpinner();
-    getMeaning()
-    // setTimeout(() => {
-    //   setWord(inputs)
-    //     // clearView();
-    //     getMeaning(inputs);
-    //     // setInputs('')
-    // }, 500);
-  }
+    searchResults(search)
+  };
+
+  console.log(word);
 
   return (
     <>
       <div className="container bg-gray-100 h-screen w-screen flex items-center justify-center min-w-full">
         <section className="bg-white p-8 rounded shadow-lg w-width">
-            <div className="form">
-                <form className="flex justify-between mb-10">
-                    <input 
-                      name='search' 
-                      value={inputs} 
-                      onChange={handleChange}
-                      type="text" 
-                      className="outline-none border-b-2 border-b-red-200 w-44" 
-                    />
-                    <button onClick={searchFunction} className="search bg-red-200 rounded px-10 py-2 text-black font-bold">Search</button>
-                </form>
-            </div>
-            <div className="results">
-                <audio id="sound"></audio>
-                {word.length > 0 
-                ? <Markup datas={word} playSound={playSound} />
-                : <div className="text-center">
-                    <p className="text-gray-400">Enter a word to begin search.</p>
-                  </div>  
-                }
-            </div>
+          <div className="form">
+            <form className="flex justify-between mb-10">
+              <input
+                name="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                type="text"
+                className="outline-none border-b-2 border-b-red-200 w-44"
+              />
+              <button
+                onClick={searchFunction}
+                className="search bg-red-200 rounded px-10 py-2 text-black font-bold"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="results">
+            <audio id="sound"></audio>
+            {word ? 
+              <Markup data={word}/>
+             : 
+              <div className="text-center">
+                <p className="text-gray-400">Enter a word to begin search.</p>
+              </div>
+            }
+          </div>
         </section>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Markup.jsx
+++ src/components/Markup.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
 
-function Markup({datas, playSound}) {
-    const word = datas.word;
-    const partOfSpeech = datas.meanings[0].partOfSpeech;
-    const phonetic = datas.phonetic? datas.phonetic: ""
-    const meaning = datas.meanings[0].definitions[0].definition;
-    const example = datas.meanings[0].definitions[0].example || ""
+function Markup({data}) {
+    console.log(data);
     // sound.setAttribute("src", `${data.phonetics[0].audio}`)
-    // // console.log(sound);  
-    // console.log(datas, playSound);
+    // console.log(sound);  
   return (
     <>
         <div className="results--header flex justify-between">
-            <h3 className="text-md font-bold text-gray-700">{word}</h3>
+            <h3 className="text-md font-bold text-gray-700">{data.word}</h3>
             <button className="sound--btn" onClick={() => playSound()}>
                 <i className="fa fa-microphone" aria-hidden="true"></i>
             </button>
         </div>
         <div className="part--of--speech text-sm text-gray-400 flex mb-8">
-            <p className="mr-2">{partOfSpeech}</p>
-            <span>{phonetic}</span>
+            <p className="mr-2">{data.meanings.partOfSpeech}</p>
+            <span>{data.phonetic? data.phonetic: ""}</span>
         </div>
         <div className="results--definition text-gray-600 mb-4 text-left">
-            <p>{meaning}</p>
+            <p>{data.meanings[0].definitions[0].definition}</p>
         </div>
         <div className="results--sentence border-l-4 pl-2 pr-12 text-left">
-            <p>{example} </p>
+            <p>{data.meanings[0].definitions[0].example || ""} </p>
         </div>
     </>
   )
